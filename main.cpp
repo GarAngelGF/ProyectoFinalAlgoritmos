a50c1398be32d5aed34fbdad082cdafda2928311
GarAngelGF <agarciaf2102@alumno.ipn.mx>
2025-12-04 08:23:57 -0600

    add FuncionesProFinal.cpp and FuncionesProFinal.h

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    cout << "Proyecto final de Algoritmos Compuracionales" << "Equipo 6:" << endl;
+    cout << "Proyecto final de Algoritmos Compuracionales" << "Equipo 6: " << endl;
 
 
     return 0;
